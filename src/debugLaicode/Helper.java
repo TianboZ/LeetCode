6bc7b56f5f2679a11d8d53c7acc93cb13045bab2
zongtb <zongtb@126.com>
2018-06-06 01:40:21 -0700

    keep fight

--- src/debugLaicode/Helper.java
+++
@@ -1,9 +0,0 @@
-package debugLaicode;
-
-public class Helper {
-    public static void printIntArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-}
